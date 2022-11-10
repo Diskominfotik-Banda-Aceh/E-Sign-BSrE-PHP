ae4f48d863d9ce273b5aeff868ce158cd30be281
Rayhan Yulanda <rayhanyulanda@gmail.com>
2022-11-10 10:51:10 +0700

    Fix | Native php

--- src/ESignBsreNative.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace DiskominfotikBandaAceh\ESignBsreNative;
-
-class ESignBsreNative
-{
-    // Build your next great package.
-}
--- src/EsignBsrePhp.php
+++ src/EsignBsrePhp.php
@@ -4,5 +4,21 @@ namespace DiskominfotikBandaAceh\EsignBsrePhp;
 
 class EsignBsrePhp
 {
-    // Build your next great package.
+    private $http;
+    private $url;
+
+    public function __construct($url=null, $username=null, $password=null){
+        $this->url = $url;
+        $this->http = Http::withBasicAuth($username, $password);
+    }
+
+    public function sign($var = null)
+    {
+        
+    }
+
+    public function verification($var = null)
+    {
+        
+    }
 }
